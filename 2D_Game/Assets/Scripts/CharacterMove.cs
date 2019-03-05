542d6f4e4d6e7d7ddbc1a0ab975e1940692df84b
Robert <robertktalbot@hotmail.com>
2019-03-04 17:16:13 -0700

    Imported Coin Art, replaced CoinTest with Coin, and fixed Jump by adding GroundCheck

--- 2D_Game/Assets/Scripts/CharacterMove.cs
+++ 2D_Game/Assets/Scripts/CharacterMove.cs
@@ -10,12 +10,18 @@ public class CharacterMove : MonoBehaviour {
     
     // player grounded variables
     private bool grounded;
+    public Transform groundCheck;
+    public float groundCheckRadius;
+    public LayerMask whatIsGround;
     
 	// Use this for initialization
 	void Start () {
-        // Default to true on grounded
-		grounded = true;
+    
 	}
+    
+    void FixedUpdate () {
+        grounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, whatIsGround);
+    }
 	
 	// Update is called once per frame
 	void Update () {
@@ -27,10 +33,14 @@ public class CharacterMove : MonoBehaviour {
             GetComponent<Rigidbody2D>().velocity = new Vector2(-moveSpeed, GetComponent<Rigidbody2D>().velocity.y);
         }
         
-        // y axis movement
+        // jump movement
         if (Input.GetKeyDown(KeyCode.W) && grounded ) { //jump
-            GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x, jumpHeight);
+            Jump();
         }
         
 	}
+    
+    void Jump () {
+        GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x, jumpHeight);
+    }
 }
---
+++ 2D_Game/Assets/Scripts/Function.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Function : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+//		print(Sum(7,8));
+        
+	}
+	
+    // does addition of two number by calling this function
+//    int Sum (int a, int b) {
+//        return a + b;
+//    }
+    
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
