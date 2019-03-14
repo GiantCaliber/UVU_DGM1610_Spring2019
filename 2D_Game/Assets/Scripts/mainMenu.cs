178a88b0f6f6007aec889171ccbfe33867064dab
Robert <robertktalbot@hotmail.com>
2019-03-13 18:12:43 -0600

    Created Menu UI, Imported Rock

---
+++ 2D_Game/Assets/Scripts/mainMenu.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class mainMenu : MonoBehaviour {
+    // Indexed level to load 
+    public int levelToLoad;
+
+	// loads level stored in levelToLoad variable
+	public void LoadLevel() {
+        SceneManager.LoadScene(levelToLoad);
+    }
+    
+    // This function quits game when activated
+    public void LevelExit() {
+        Application.Quit();
+    }
+}
