1ee210f8906988d54d9dbe214b643b35a891a6a6
Robert <robertktalbot@hotmail.com>
2019-03-25 17:34:47 -0600

    Added Enemy Movement, Tested Loops

---
+++ 2D_Game/Assets/Scripts/Loops.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Loops : MonoBehaviour {
+
+    private int bottles = 100;
+
+	// Use this for initialization
+	void Start () {
+
+        // For Loop test
+        for ( int i = 0; i < bottles; i++ ) {
+            print( ( i + 1 ) + " bottle(s)" );
+        }
+
+        // While Loop test
+        while ( bottles >= 0 ) {
+            print(bottles + " trash remaining");
+            bottles--;
+            if (bottles == -1) {
+                print("All trash has been eliminated");
+            }
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
