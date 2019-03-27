e7ef5c5c246ff88af6b8e9e533c220a76bb39132
Robert <robertktalbot@hotmail.com>
2019-03-27 17:17:48 -0600

    Started working on Kill and LevelManager Script

---
+++ 2D_Game/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour {
+
+    public GameObject currentCheckPoint;
+    private Rigidbody2D pcRigid;
+
+    private GameObject player;
+
+    // Particles
+    public GameObject deathParticle;
+    public GameObject respawnParticle;
+
+    // Respawn Delay
+    public float respawnDelay;
+
+    // Point Penalty on Death
+    public int pointPenaltyOnDeath;
+
+    // Store Gravity Value
+    private float GravityStore;
+
+    // Use this for initialization
+	void Start () {
+        pcRigid = GameObject.Find("PC").GetComponent<Rigidbody2D>();
+        player = GameObject.Find("Player");
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
--- 2D_Game/Assets/Scripts/Loops.cs
+++ 2D_Game/Assets/Scripts/Loops.cs
@@ -4,12 +4,12 @@ using UnityEngine;
 
 public class Loops : MonoBehaviour {
 
-    private int bottles = 100;
+    // private int bottles = 100;
 
 	// Use this for initialization
 	void Start () {
 
-        // For Loop test
+        /* For Loop test
         for ( int i = 0; i < bottles; i++ ) {
             print( ( i + 1 ) + " bottle(s)" );
         }
@@ -21,8 +21,27 @@ public class Loops : MonoBehaviour {
             if (bottles == -1) {
                 print("All trash has been eliminated");
             }
+        } */
+
+        // Do While Loop
+        // bool shouldContinue = false;
+
+        //do { // always will run once, regardless if while statement is false. will loop if while statement is true
+        //    print( "Hello World" );
+        //} while ( shouldContinue == true );
+
+        // ForEach Loop
+        string[] strings = new string[3];
+
+        strings[0] = "First";
+        strings[1] = "Second";
+        strings[2] = "Third";
+
+        // runs through the array, item is used to store array data
+        foreach ( string item in strings ) {
+            print (item);
         }
-	}
+    }
 	
 	// Update is called once per frame
 	void Update () {
