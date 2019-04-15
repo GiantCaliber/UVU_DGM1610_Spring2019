c9e8314053a723280d6e7ec4ac0aa2f6904c52b6
Robert <robertktalbot@hotmail.com>
2019-04-15 17:43:33 -0600

    Added Platform, Started Animation Setup

--- 2D_Game/Assets/Scripts/CharacterMove.cs
+++ 2D_Game/Assets/Scripts/CharacterMove.cs
@@ -14,12 +14,22 @@ public class CharacterMove : MonoBehaviour {
     public float groundCheckRadius;
     public LayerMask whatIsGround;
 
+    // player scale for flip
     private Vector3 scale;
+
+    // non-stick player
+    public float moveVelocity;
+
+    // animator
+    public Animator animator;
     
 	// Use this for initialization
 	void Start () {
+        animator = GetComponent<Animator>();
         scale = transform.localScale;
-	}
+        animator.SetBool("isWalking", false);
+        animator.SetBool("isJumping", false);
+    }
     
     void FixedUpdate () {
         grounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, whatIsGround);
@@ -27,28 +37,47 @@ public class CharacterMove : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		// x axis movement
-        if (Input.GetKey(KeyCode.D)) { //right movement
-            GetComponent<Rigidbody2D>().velocity = new Vector2(moveSpeed, GetComponent<Rigidbody2D>().velocity.y);
-        }
-        else if (Input.GetKey(KeyCode.A)) { //left movement
-            GetComponent<Rigidbody2D>().velocity = new Vector2(-moveSpeed, GetComponent<Rigidbody2D>().velocity.y);
-        }
-        
+
         // jump movement
-        if (Input.GetKeyDown(KeyCode.W) && grounded ) { //jump
+        if (Input.GetKeyDown(KeyCode.W) && grounded) { //jump
             Jump();
         }
-        // player flip
-        if (GetComponent<Rigidbody2D>().velocity.x > 0)
+
+        // non-stick player
+        moveVelocity = 0f;
+
+        // x axis movement
+        if (Input.GetKey(KeyCode.D)) { //right movement
+            GetComponent<Rigidbody2D>().velocity = new Vector2(moveSpeed, GetComponent<Rigidbody2D>().velocity.y);
+            moveVelocity = moveSpeed;
+            animator.SetBool("isWalking", true);
             transform.localScale = new Vector3(scale.x, scale.y, scale.z);
-        else if (GetComponent<Rigidbody2D>().velocity.x < 0)
+        }
+        else if (Input.GetKeyUp(KeyCode.D))
+            animator.SetBool("isWalking", false);
+
+        if (Input.GetKey(KeyCode.A)) { //left movement
+            GetComponent<Rigidbody2D>().velocity = new Vector2(-moveSpeed, GetComponent<Rigidbody2D>().velocity.y);
+            moveVelocity = -moveSpeed;
+            animator.SetBool("isWalking", true);
             transform.localScale = new Vector3(-scale.x, scale.y, scale.z);
+        }
+        else if (Input.GetKeyUp(KeyCode.A))
+            animator.SetBool("isWalking", false);
+
+
+
+        // player flip
+        //if (GetComponent<Rigidbody2D>().velocity.x > 0)
+            
+        //else if (GetComponent<Rigidbody2D>().velocity.x < 0)
+            
 
         
 	}
     
     void Jump () {
         GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x, jumpHeight);
+        animator.SetBool("isJumping", true);
     }
 }
